6684239a47d49b86f2ded67703dd10ebe7371bf7
Your Name <you@example.com>
2021-05-05 20:32:42 +0200

    latest for today

--- script.js
+++ script.js
@@ -8,14 +8,19 @@ window.onresize = resize;
 function resize(){
     
     if(screen.availHeight > screen.availWidth){
+        //potrait
         document.getElementById("landscape_cover").classList.add("notVisible")
         document.getElementById("landscape_menetrend").classList.add("notVisible")
+        document.getElementById("backbtnPotrait").classList.remove("notVisible")
+        
+        
         document.getElementById("col2").style.width="100%"
         
     }
     if(screen.availHeight < screen.availWidth){
         document.getElementById("landscape_cover").classList.remove("notVisible")
         document.getElementById("landscape_menetrend").classList.remove("notVisible")
+        document.getElementById("backbtnPotrait").classList.add("notVisible")
         document.getElementById("col2").style.width="50%"
     }
     // Announce the new orientation number
