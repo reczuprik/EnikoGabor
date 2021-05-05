e3c8fb3b5723668cee91820b6cb05ab2baa7b2ad
Your Name <you@example.com>
2021-05-05 18:52:24 +0200

    lot of changes

---
+++ script.js
@@ -0,0 +1,23 @@
+window.onload = resize;
+
+function myFunction(){
+    document.getElementById("bal").classList.toggle("page1Flipped")
+}
+
+window.onresize = resize;
+function resize(){
+    
+    if(screen.availHeight > screen.availWidth){
+        document.getElementById("landscape_cover").classList.add("notVisible")
+        document.getElementById("landscape_menetrend").classList.add("notVisible")
+        document.getElementById("col2").style.width="100%"
+        
+    }
+    if(screen.availHeight < screen.availWidth){
+        document.getElementById("landscape_cover").classList.remove("notVisible")
+        document.getElementById("landscape_menetrend").classList.remove("notVisible")
+        document.getElementById("col2").style.width="50%"
+    }
+    // Announce the new orientation number
+    
+}
